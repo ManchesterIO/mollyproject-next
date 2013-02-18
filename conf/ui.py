40881eec42d047eced0e49c7911738841498270d
Chris Northwood <chris@pling.org.uk>
2013-02-18 14:34:30 +0000

    Make static files and template inheritance work in a similar way as Molly 1.x

---
+++ conf/ui.py
@@ -0,0 +1,14 @@
+# Configuration file for the HTML5 UI
+
+# Set these to be the values of your Molly REST server
+MOLLY_API_HOSTNAME = 'localhost'
+MOLLY_API_PORT = 8000
+
+# This is the location of any custom templates you wish to use
+#TEMPLATE_DIR = ''
+
+# This is the location of any custom assets you wish to use
+#OVERRIDE_ASSET_DIR = ''
+
+# This is the place where assets should be served from
+ASSET_DIR = '/opt/molly/static'
--- molly/ui/html5/server.py
+++ molly/ui/html5/server.py
@@ -1,29 +1,84 @@
+from shutil import copyfile
 import os
 
 from flask import Flask
 from flask.ext.babel import Babel
-from jinja2 import PackageLoader
+from flask.ext.cache import Cache
+from jinja2 import PackageLoader, ChoiceLoader, PrefixLoader, FileSystemLoader, MemcachedBytecodeCache
 
 from molly.ui.html5.components.factory import ComponentFactory
 from molly.ui.html5.page_decorators.page_decorator_factory import PageDecoratorFactory
 from molly.ui.html5.request_factory import HttpRequestFactory
 from molly.ui.html5.router import Router
 
-flask_app = Flask(__name__)
-Babel(flask_app)
+def init_flask():
+    flask_app = Flask(__name__)
+    flask_app.config.from_envvar('MOLLY_UI_SETTINGS')
+    Babel(flask_app)
+    return flask_app
+flask_app = init_flask()
 
-request_factory = HttpRequestFactory(hostname='localhost', port=8000)
-component_factory = ComponentFactory()
-page_decorator_factory = PageDecoratorFactory()
-router = Router(request_factory, component_factory, page_decorator_factory)
 
-flask_app.add_url_rule('/', 'homepage', view_func=router)
-flask_app.add_url_rule('/<path:path>', 'main', view_func=router)
+def init_molly(flask_app, api_hostname, api_port):
+    request_factory = HttpRequestFactory(hostname=api_hostname, port=api_port)
+    component_factory = ComponentFactory()
+    page_decorator_factory = PageDecoratorFactory()
 
-flask_app.jinja_loader = PackageLoader('molly.ui.html5', 'templates')
+    router = Router(request_factory, component_factory, page_decorator_factory)
+
+    flask_app.add_url_rule('/', 'homepage', view_func=router)
+    flask_app.add_url_rule('/<path:path>', 'main', view_func=router)
+
+
+def configure_template_loader(flask_app, template_dir, cache):
+    if template_dir:
+        loaders = [FileSystemLoader(template_dir)]
+    else:
+        loaders = []
+
+    flask_app.jinja_loader = ChoiceLoader(loaders + [
+        PackageLoader('molly.ui.html5', 'templates'),
+        PrefixLoader({'molly_default': PackageLoader('molly.ui.html5', 'templates')})
+    ])
+
+    if cache:
+        flask_app.jinja_env.bytecode_cache = MemcachedBytecodeCache(cache)
+
+
+def collect_static(override_location, output_location, debug):
+    asset_paths = [os.path.join(os.path.abspath(os.path.dirname(__file__)), 'assets')]
+    if override_location:
+        asset_paths.append(override_location)
+
+    for root in asset_paths:
+        for dirpath, dirnames, filenames in os.walk(root):
+            for filename in filenames:
+                source = os.path.join(dirpath, filename)
+                destination = os.path.join(output_location, dirpath[len(root)+1:], filename)
+                print source, destination
+                if not os.path.exists(os.path.dirname(destination)):
+                    os.makedirs(os.path.dirname(destination))
+                if debug:
+                    if os.path.exists(destination):
+                        os.remove(destination)
+                    os.symlink(source, destination)
+                else:
+                    copyfile(source, destination)
+
 
 def start_debug(address=None):
+    cache = Cache(flask_app)
+    init_molly(
+        flask_app,
+        flask_app.config.get('MOLLY_API_HOSTNAME', 'localhost'),
+        flask_app.config.get('MOLLY_API_PORT', 8000)
+    )
+    configure_template_loader(flask_app, flask_app.config.get('TEMPLATE_DIR'), cache)
+
     flask_app.debug = True
-    flask_app.static_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), 'assets'))
+    flask_app.static_folder = flask_app.config.get('ASSET_DIR')
     flask_app.static_url_path = 'static'
+
+    collect_static(flask_app.config.get('OVERRIDE_ASSET_DIR'), flask_app.static_folder, True)
+
     flask_app.run(debug=True, host=address, port=8002)
