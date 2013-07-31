80eede7ffaf824d0d5a4d29a3b0eee2504f045ac
Chris Northwood <chris@pling.org.uk>
2013-07-31 21:22:51 +0200

    Add an endpoint for doing nearby searches

--- molly/apps/places/endpoints.py
+++ molly/apps/places/endpoints.py
@@ -1,4 +1,5 @@
-from flask import url_for
+from flask import url_for, redirect
+from shapely.geometry import Point
 from werkzeug.exceptions import abort
 
 from molly.apps.common.endpoints import Endpoint
@@ -20,3 +21,27 @@ class PointOfInterestEndpoint(Endpoint):
                 'href': self._href(slug),
                 'poi': poi._asdict()
             })
+
+
+class NearbySearchEndpoint(Endpoint):
+    def __init__(self, instance_name, poi_service):
+        self._poi_service = poi_service
+        self._href = lambda lat, lon: url_for(instance_name + '.nearby', lat=lat, lon=lon, _external=True)
+        self._poi_href = lambda slug: url_for(instance_name + '.poi', slug=slug, _external=True)
+
+    def get(self, lat, lon):
+        fixed_lat, fixed_lon = map(lambda l: round(l, 5), [lat, lon])
+        if (fixed_lat, fixed_lon) != (lat, lon):
+            return redirect(self._href(fixed_lat, fixed_lon))
+        else:
+            return self._json_response({
+                'self': 'http://mollyproject.org/apps/places/points-of-interest',
+                'points_of_interest': self._fetch_points_of_interest(fixed_lat, fixed_lon)
+            })
+
+    def _fetch_points_of_interest(self, lat, lon):
+        pois = self._poi_service.search_nearby(Point(lon, lat))
+        pois = map(lambda poi: poi._asdict(), pois)
+        for poi in pois:
+            poi['href'] = self._poi_href(poi['slug'])
+        return pois
--- tests/molly/apps/places/test_endpoints.py
+++ tests/molly/apps/places/test_endpoints.py
@@ -1,11 +1,13 @@
 import json
+import urllib2
 from flask import Flask
 from mock import Mock
+from shapely.geometry import Point
 import unittest2 as unittest
 
 from werkzeug.exceptions import NotFound
 
-from molly.apps.places.endpoints import PointOfInterestEndpoint
+from molly.apps.places.endpoints import PointOfInterestEndpoint, NearbySearchEndpoint
 from molly.apps.places.models import PointOfInterest
 
 
@@ -39,3 +41,45 @@ class PointOfInterestEndpointTest(unittest.TestCase):
         app.add_url_rule('/poi/<slug>', 'testplaces.poi', self._endpoint.get)
         with app.test_request_context('/', headers=[('Accept', 'application/json')]):
             return json.loads(self._endpoint.get('foo:bar').data)
+
+
+class NearbySearchEndpointTest(unittest.TestCase):
+
+    def setUp(self):
+        self._poi_service = Mock()
+        self.maxDiff = None
+
+    def _make_request(self, lat, lon):
+        endpoint = NearbySearchEndpoint('testplaces', self._poi_service)
+        app = Flask(__name__)
+        app.add_url_rule('/nearby/<float:lat>,<float:lon>', 'testplaces.nearby', endpoint.get)
+        app.add_url_rule('/poi/<slug>', 'testplaces.poi', None)
+        with app.test_request_context('/', headers=[('Accept', 'application/json')]):
+            response = endpoint.get(lat, lon)
+        return response
+
+    def test_overly_precise_requests_are_rounded_down(self):
+        response = self._make_request(10.123456789, 15.987654321)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(
+            'http://localhost/nearby/10.12346,15.98765',
+            urllib2.unquote(dict(response.headers).get('Location'))
+        )
+
+    def test_search_is_passed_to_service(self):
+        self._poi_service.search_nearby.return_value = []
+        response = self._make_request(54.5, 0.6)
+        self.assertEqual({
+            'self': 'http://mollyproject.org/apps/places/points-of-interest',
+            'points_of_interest': []
+        }, json.loads(response.data))
+        point = self._poi_service.search_nearby.call_args[0][0]
+        self.assertEqual((0.6, 54.5), (point.x, point.y))
+
+    def test_search_results_are_correctly_serialised(self):
+        self._poi_service.search_nearby.return_value = [
+            PointOfInterest(slug='foo:bar', telephone_number='01818118181')
+        ]
+        response = json.loads(self._make_request(54.5, 0.6).data)
+        self.assertEqual('http://localhost/poi/foo:bar', response['points_of_interest'][0]['href'])
+        self.assertEqual('01818118181', response['points_of_interest'][0]['telephone_number'])
