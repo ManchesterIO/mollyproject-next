2d7fed068f2a3342e533b6ef41ba546cd0c7309e
Chris Northwood <chris@pling.org.uk>
2013-09-28 23:24:37 +0100

    Add nearby search button to homepage

--- molly/ui/html5/assets/js/modules/places/nearby-search.js
+++ molly/ui/html5/assets/js/modules/places/nearby-search.js
@@ -1,7 +1,38 @@
-define([], function() {
+define(['vendors/geoPosition'], function(geoPosition) {
     "use strict";
 
-    return function(elem) {
-        elem.style.display = 'block';
+    var NearbySearch = function(elem) {
+
+        var nearbySearch = this;
+        var link = elem.querySelector('button');
+
+        var determineUrl = function(lat, lon) {
+            return link.dataset.href.replace("{lat}", lat).replace("{lon}", lon);
+        };
+
+        var handleNearbySearchClick = function(ev) {
+            link.disabled = true;
+            link.innerText = 'Determining your location';
+            link.classList.add('loading');
+            geoPosition.getCurrentPosition(
+                function(position) {
+                    nearbySearch.setWindowLocation(determineUrl(position.coords.latitude, position.coords.longitude));
+                },
+                function(err) {
+                    link.classList.remove('loading');
+                    link.classList.add('failed');
+                    link.innerText = 'Failed to determine your location';
+                });
+        };
+
+        if (geoPosition.init()) {
+            elem.style.display = 'block';
+            link.addEventListener('click', handleNearbySearchClick);
+        }
     };
+    NearbySearch.prototype.setWindowLocation = function(url) {
+        window.location = url;
+    };
+
+    return function(elem) { return new NearbySearch(elem); };
 });
--- tests/molly/ui/html5/js/specs/places/nearby-search.js
+++ tests/molly/ui/html5/js/specs/places/nearby-search.js
@@ -1,20 +1,65 @@
-require(['molly/places/nearby-search'], function(nearbySearch) {
+define(['molly/places/nearby-search', 'sinon', 'vendors/geoPosition'], function(nearbySearch, sinon, geoPosition) {
     "use strict";
 
-    var div;
-
-    beforeEach(function(){
-        div = document.createElement('div');
-        var a = document.createElement('a');
-        a.href = '/places/{lat},{lon}';
-        div.appendChild(a);
-    });
-
     describe("nearby search is available on devices with geolocation", function() {
-        it("shows the nearby link on devices with JavaScript", function() {
+
+        var div, link, setWindowLocation;
+
+        beforeEach(function() {
+            div = document.createElement('div');
             div.style.display = 'none';
+
+            link = document.createElement('button');
+            link.dataset.href = '/places/{lat},{lon}';
+            div.appendChild(link);
+
+            sinon.stub(geoPosition, "init");
+            sinon.stub(geoPosition, "getCurrentPosition");
+            setWindowLocation = undefined;
+            geoPosition.init.returns(true);
+        });
+
+        afterEach(function() {
+            geoPosition.init.restore();
+            geoPosition.getCurrentPosition.restore();
+            if (setWindowLocation !== undefined ) {
+                setWindowLocation.restore();
+            }
+        });
+
+        it("shows the nearby link on devices with JavaScript", function() {
             nearbySearch(div);
             expect(div.style.display).toBe('block');
+        });
+
+        it("does not show the nearby link on devices without JavaScript", function() {
+            geoPosition.init.returns(false);
+            nearbySearch(div);
+            expect(div.style.display).toBe('none');
+        });
+
+        it("changes to a loading spinner when clicked on", function() {
+            nearbySearch(div);
+            link.dispatchEvent(new window.MouseEvent('click'));
+            expect(link.classList.contains('loading')).toBeTruthy();
+            expect(link.innerText).toBe("Determining your location");
+            expect(link.disabled).toBeTruthy();
+        });
+
+        it("when geolocation returns result, user is navigated to page", function() {
+            geoPosition.getCurrentPosition.callsArgWith(0, {coords: {latitude: 12.3, longitude: 45.6}});
+            setWindowLocation = sinon.stub(nearbySearch(div), 'setWindowLocation');
+            link.dispatchEvent(new window.MouseEvent('click'));
+            expect(setWindowLocation.calledWith('/places/12.3,45.6')).toBeTruthy();
+        });
+
+        it("shows an error message when geolocation fails", function() {
+            geoPosition.getCurrentPosition.callsArgWith(1, {code: 1});
+            nearbySearch(div);
+            link.dispatchEvent(new window.MouseEvent('click'));
+            expect(link.classList.contains('loading')).toBeFalsy();
+            expect(link.classList.contains('failed')).toBeTruthy();
+            expect(link.innerText).toBe("Failed to determine your location");
         });
     });
 
